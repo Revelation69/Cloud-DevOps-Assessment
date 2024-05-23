20f9a8ea6c01980ecd7745d9710cf7b434c68040
Revelation69 <mickelmicho6991@gmail.com>
2024-05-23 11:37:04 +0100

    Fix backend.tf structure

--- backend.tf
+++ backend.tf
@@ -1,5 +1,8 @@
 terraform {
-  backend "remote" {
-
+  backend "s3" {
+    bucket         = "cloud-devops-assessment-s3-bucket"
+    key            = "envs/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "cloud-devops-assessment"
   }
 }
