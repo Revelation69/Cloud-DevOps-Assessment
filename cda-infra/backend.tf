bae1b0f576fe75784f5e482cb74a763414f8495a
Revelation69 <mickelmicho6991@gmail.com>
2024-05-23 10:58:10 +0100

    added gitworkflow

--- cda-infra/backend.tf
+++ cda-infra/backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "cloud-devops-assessment-s3"
-    key            = "cloud-devops-assessment/terraform/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "cloud-devops-assessment"
+  backend "remote" {
+
   }
 }
